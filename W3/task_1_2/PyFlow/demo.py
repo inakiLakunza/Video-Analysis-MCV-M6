0dd7d4b3a2833d7cbe234db2c2823da9e6564a58
ggcr <ggcristian@icloud.com>
2024-03-09 23:19:30 +0100

    Used an external lib to read gt

--- W3/task_1_2/PyFlow/demo.py
+++ W3/task_1_2/PyFlow/demo.py
@@ -11,44 +11,39 @@ import argparse
 import pyflow
 import cv2
 
-parser = argparse.ArgumentParser(
-    description='Demo for python wrapper of Coarse2Fine Optical Flow')
-parser.add_argument(
-    '-viz', dest='viz', action='store_true',
-    help='Visualize (i.e. save) output of flow.')
-args = parser.parse_args()
 
-im1 = cv2.imread('../../data_stereo_flow/training/colored_0/000045_10.png', cv2.IMREAD_GRAYSCALE)
-im1 = im1.reshape([im1.shape[0], im1.shape[1], 1])
-im2 = cv2.imread('../../data_stereo_flow/training/colored_0/000045_11.png', cv2.IMREAD_GRAYSCALE)
-im2 = im2.reshape([im2.shape[0], im2.shape[1], 1])
-im1 = im1.astype(float) / 255.
-im2 = im2.astype(float) / 255.
+def compute_flow_pipeline(im1_path: str, im2_path: str, channels: int = 3, **kwargs):
+    # read images
+    im1 = cv2.imread(im1_path, cv2.COLOR_BGR2RGB)
+    im2 = cv2.imread(im2_path, cv2.COLOR_BGR2RGB)
+    if channels == 1:
+        im1 = cv2.imread(im1_path, cv2.IMREAD_GRAYSCALE)
+        im2 = cv2.imread(im2_path, cv2.IMREAD_GRAYSCALE)
+        im1 = im1.reshape([im1.shape[0], im1.shape[1], 1])
+        im2 = im2.reshape([im2.shape[0], im2.shape[1], 1])
+    
+    im1 = im1.astype(float) / 255.
+    im2 = im2.astype(float) / 255.
 
-# Flow Options:
-alpha = 0.012
-ratio = 0.75
-minWidth = 20
-nOuterFPIterations = 7
-nInnerFPIterations = 1
-nSORIterations = 30
-colType = 1  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
+    # params
+    alpha = float(kwargs['alpha']) # 0.012
+    ratio = float(kwargs['ratio']) # 0.75
+    minWidth = int(kwargs['minWidth']) # 20
+    nOuterFPIterations = int(kwargs['nOuterFPIterations']) #7
+    nInnerFPIterations = int(kwargs['nInnerFPIterations']) #1
+    nSORIterations = int(kwargs['nSORIterations']) #30
+    colType = 0 if channels > 1 else 1
 
-s = time.time()
-u, v, im2W = pyflow.coarse2fine_flow(
-    im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
-    nSORIterations, colType)
-flow = np.dstack((u, v))
-np.save('../results/PyFlow.npy', flow)
-
-e = time.time()
-print('Time Taken: %.2f seconds for image of size (%d, %d, %d)' % (
-    e - s, im1.shape[0], im1.shape[1], im1.shape[2]))
-flow = np.concatenate((u[..., None], v[..., None]), axis=2)
+    # compute flow
+    u, v, _ = pyflow.coarse2fine_flow(
+        im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
+        nSORIterations, colType)
+    return np.dstack((u, v))
 
 
-if args.viz:
-    import cv2
+def visualize_flow(im1_path: str, flow):
+    im1 = cv2.imread(im1_path, cv2.IMREAD_GRAYSCALE)
+    im1 = im1.reshape([im1.shape[0], im1.shape[1], 1])
     hsv = np.zeros(im1.shape, dtype=np.uint8)
     hsv[:, :, 0] = 255
     hsv[:, :, 1] = 255
@@ -56,6 +51,4 @@ if args.viz:
     hsv[..., 0] = ang * 180 / np.pi / 2
     hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
     rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    print("Writting...")
-    cv2.imwrite('./outFlow_result.png', rgb)
-    cv2.imwrite('examples/car2Warped.jpg', im2W[:, :, ::-1] * 255)
+    return rgb
